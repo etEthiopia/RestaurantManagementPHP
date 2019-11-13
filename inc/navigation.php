d1ec9927e8a17bfe959f34dd83b5369a81c6c11c
etEthiopia <daginegussu@gmail.com>
2019-11-13 10:04:38 +0300

    nav

--- inc/navigation.php
+++ inc/navigation.php
@@ -7,19 +7,19 @@
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav ml-auto mr-auto">
       <li class="nav-item <?php if ($active=='index') { echo "active"; } ?>">
-        <a class="nav-link" href="../index.php">ቤት<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="../index.php">HOME<span class="sr-only">(current)</span></a>
       </li>
 
       <li class="nav-item dropdown <?php if (($active=='login') || ($active=='register')) { echo "active"; } ?>">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        መለያ
+        ACCOUNT
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <?php  if(isset($_SESSION['admin'])): ?>
-                <a class="dropdown-item" href="../views/logout.php">ዘግተው ይውጡ</a>
+                <a class="dropdown-item" href="../views/logout.php">LOG OUT</a>
           <?php  else: ?>
-          <a class="dropdown-item" href="../views/login.php">በመለያ ግባ</a>
-          <a class="dropdown-item" href="../views/register.php">መዝግብ</a>
+          <a class="dropdown-item" href="../views/login.php">LOG IN</a>
+          <a class="dropdown-item" href="../views/register.php">REGISTER</a>
         <?php  endif; ?>
 
         </div>
@@ -28,11 +28,11 @@
     <?php  if(isset($_SESSION['admin']) && $_SESSION['admin']==0): ?>
   	<li class="nav-item dropdown <?php if (($active=='reservation') || ($active=='history')) { echo "active"; } ?>">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        ቦታ ማስያዝ
+        RESERVE A SPOT
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="reservation.php">አዲስ ቦታ ማስያዝ</a>
-          <a class="dropdown-item" href="history.php">የቆዩ ቦታዎች</a>
+          <a class="dropdown-item" href="reservation.php">NEW RESERVATION</a>
+          <a class="dropdown-item" href="history.php">DONE RESERVATIONS</a>
         </div>
       </li>
 
@@ -42,10 +42,10 @@
           አስተዳደር
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="../admin/dashboard.php">የመጠባበቂያ ሁኔታ</a>
-            <a class="dropdown-item" href="../admin/tab_management.php">ጠረጴዛን ማስተዳደር</a>
-            <a class="dropdown-item" href="../admin/date_management.php">የጊዜ ማኔጅመንት</a>
-            <a class="dropdown-item" href="../admin/admin_reservations.php">የአስተዳደር መጠባበቂያ</a>
+            <a class="dropdown-item" href="../admin/dashboard.php">WAITING LIST</a>
+            <a class="dropdown-item" href="../admin/tab_management.php">MANAGING TABLES</a>
+            <a class="dropdown-item" href="../admin/date_management.php">MANAGING HOURS</a>
+            <a class="dropdown-item" href="../admin/admin_reservations.php">MANAGEMENT SETTINGS</a>
           </div>
         </li>
     <?php  endif; ?>
@@ -57,7 +57,7 @@
               <?= $_SESSION['fname']; ?>&nbsp;<?= $_SESSION['lname']; ?>
             </a>
             <div class="dropdown-menu" aria-labelledby="LogoutDropdown">
-              <a class="dropdown-item" href="../views/logout.php">ዘግተው ይውጡ</a>
+              <a class="dropdown-item" href="../views/logout.php">LOG OUT</a>
 
             </div>
           </li>
