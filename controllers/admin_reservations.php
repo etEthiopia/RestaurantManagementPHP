246c6fac1e14b52040abf7f3d68a65d911731556
etEthiopia <daginegussu@gmail.com>
2019-11-13 10:20:50 +0300

    admin res

--- controllers/admin_reservations.php
+++ controllers/admin_reservations.php
@@ -36,7 +36,7 @@ $nameOfDay = date('D', strtotime($date));
 
     if ($day_chk->num_rows < 1):
 
-      $err_day = "በዚህ ቀን ምግብ ቤቱ ዝግ ነው።";
+      $err_day = "Sorry, We are Closed on this Day";
 
     endif;
 
