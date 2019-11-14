2ce4e86417a7e23c084d6543cc40473f97c34e7b
etEthiopia <daginegussu@gmail.com>
2019-11-14 10:35:45 +0300

    dashbo

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -14,21 +14,21 @@
 
 <div class="container pt-2 pb-5">
 
-<h4 class="text-center pb-3">የመጠባበቂያ ሁኔታ</h4>
+<h4 class="text-center pb-3">Management Settings</h4>
 
       <form id="dateform" name="dateform" action="<?= $_SERVER['PHP_SELF']; ?>" method="post" class="col-md-6 offset-md-3 pt-2 pb-3">
 
     <div class="form-group">
-    <label for="res_date">በቀን የተያዙ ቦታዎችን ይፈልጉ</label>
+    <label for="res_date">Find Day Reservations</label>
     <input type="date" name="res_date" class="form-control" id="res_date" min="<?= date("Y-m-d"); ?>" required>
 
     </div>
 
-    <button type="submit" id="submit_date" name="submit_date" class="form-control submit-btn btn btn-info d-block mr-auto ml-auto">በቀን ፈልግ</button>
+    <button type="submit" id="submit_date" name="submit_date" class="form-control submit-btn btn btn-info d-block mr-auto ml-auto">Find by Date</button>
     </form>
 
     <form id="allresults" name="allresults" action="<?= $_SERVER['PHP_SELF']; ?>" method="post" class="col-md-6 offset-md-3 mt-3 mb-5">
-  <button type="submit" id="all_reservations" name="all_reservations" class="form-control submit-btn btn btn-warning d-block mr-auto ml-auto">ሁሉም መያዣዎች</button>
+  <button type="submit" id="all_reservations" name="all_reservations" class="form-control submit-btn btn btn-warning d-block mr-auto ml-auto">All Reservations</button>
   </form>
 
 <?php if(isset($_POST['all_reservations'])):
@@ -46,12 +46,12 @@
 <thead class="thead-dark">
 <tr>
 
-<th scope="col">የመጠባበቂያ ኮድ</th>
-<th scope="col" class="text-left">ስም</th>
-<th scope="col" class="text-left">የአባት ስም</th>
-<th scope="col">የመታወቂያ ቀን</th>
-<th scope="col">የጠረጴዛ ቁጥር</th>
-<th scope="col">የሰዎች ቁጥር</th>
+<th scope="col">Reservation Code</th>
+<th scope="col" class="text-left">First Name</th>
+<th scope="col" class="text-left">Last Name</th>
+<th scope="col">Date</th>
+<th scope="col">Table Number</th>
+<th scope="col">Number of Customers</th>
 </tr>
 </thead>
 <tbody>
@@ -99,12 +99,12 @@ endif; ?>
 <thead class="thead-dark">
 <tr>
 
-<th scope="col">የመጠባበቂያ ኮድ</th>
-<th scope="col" class="text-left">ስም</th>
-<th scope="col" class="text-left">የአባት ስም</th>
-<th scope="col">የመታወቂያ ቀን</th>
-<th scope="col">የጠረጴዛ ቁጥር</th>
-<th scope="col">የሰዎች ቁጥር</th>
+<th scope="col">Reservation Code</th>
+<th scope="col" class="text-left">First Name</th>
+<th scope="col" class="text-left">Last Name</th>
+<th scope="col">Date</th>
+<th scope="col">Table Number</th>
+<th scope="col">Number of Customers</th>
 </tr>
 </thead>
 <tbody>
