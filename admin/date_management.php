4f6acbb8168012486d60b661e118535e97f99c0d
etEthiopia <daginegussu@gmail.com>
2019-11-14 10:37:44 +0300

    dat man

--- admin/date_management.php
+++ admin/date_management.php
@@ -15,12 +15,12 @@
 
 <div class="container pt-2 pb-5">
 
-<h4 class="text-center pb-3">የጊዜ አጠቃቀም</h4>
+<h4 class="text-center pb-3">Managing Hours</h4>
 
 <nav>
   <div class="nav nav-tabs" id="nav-tab" role="tablist">
-    <a class="nav-item nav-link active" id="nav-open-tab" data-toggle="tab" href="#nav-open" role="tab" aria-controls="nav-open" aria-selected="true">የስራ ሰዓት</a>
-    <a class="nav-item nav-link" id="nav-close-tab" data-toggle="tab" href="#nav-close" role="tab" aria-controls="nav-close" aria-selected="false">በዓላት</a>
+    <a class="nav-item nav-link active" id="nav-open-tab" data-toggle="tab" href="#nav-open" role="tab" aria-controls="nav-open" aria-selected="true">Working Hours</a>
+    <a class="nav-item nav-link" id="nav-close-tab" data-toggle="tab" href="#nav-close" role="tab" aria-controls="nav-close" aria-selected="false">Holidays</a>
 
   </div>
 </nav>
@@ -65,23 +65,23 @@
                 <div class="form-group">
                   <label for="day_upd">ቀን</label>
                 <select id="day_upd" name="day_upd" class="form-control" required>
-                  <option value="" disabled selected>አንድ ቀን ይምረጡ</option>
+                  <option value="" disabled selected>Choose a Date</option>
     <?php 
       require('../models/date_management.php');?>
                 </select>
                 </div>
 
               <div class="form-group">
-                <label for="opening_upd">ክፍት ከ:</label>
+                <label for="opening_upd">Open from:</label>
               <input type="time" class="form-control" id="opening_upd" name="opening_upd" required>
               </div>
 
               <div class="form-group">
-                <label for="closing_upd">ክፍት እስከ:</label>
+                <label for="closing_upd">Up to:</label>
               <input type="time" class="form-control" id="closing_upd" name="closing_upd" required>
               </div>
 
-              <button type="submit" name="date_upd" id="date_upd" class="submit-btn btn btn-dark d-block mr-auto ml-auto">ማስገባት</button>
+              <button type="submit" name="date_upd" id="date_upd" class="submit-btn btn btn-dark d-block mr-auto ml-auto">Submit</button>
               </form>
   </div>
 
@@ -94,7 +94,7 @@
                 <div class="form-group">
                   <label for="day_close">ቀን</label>
                 <select id="day_close" name="day_close_id" class="form-control" required>
-                  <option value="" selected disabled>አንድ ቀን ይምረጡ</option>
+                  <option value="" selected disabled>Choose a Date</option>
     <?php
         $days = $conn->query("SELECT id, day_name FROM days");
           if($days->num_rows > 0):
@@ -110,7 +110,7 @@
                 </select>
                 </div>
 
-              <button type="submit" name="day_close" id="day_close" class="submit-btn btn btn-dark d-block mr-auto ml-auto">ማስገባት</button>
+              <button type="submit" name="day_close" id="day_close" class="submit-btn btn btn-dark d-block mr-auto ml-auto">Submit</button>
               <?php if (isset($fail)): ?>
               <div class="alert alert-danger p-2 mt-2" role="alert"><?= $fail; ?></div>
               <?php endif; ?>
